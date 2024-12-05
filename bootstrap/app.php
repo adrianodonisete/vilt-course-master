4160b4c068dca118f30df22a6606a85d45307035
Adriano D Machado <adriano@systax.com.br>
2024-12-05 17:04:23 -0300

    login and logout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -34,6 +34,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'logout',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
