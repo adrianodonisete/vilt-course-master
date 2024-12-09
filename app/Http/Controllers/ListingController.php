991879b1dbae0780503cfe1a32317e28f163ec75
Adriano D Machado <adriano@systax.com.br>
2024-12-09 18:02:18 -0300

    pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -33,7 +33,8 @@ class ListingController extends Controller implements HasMiddleware
         return inertia(
             'Listing/Index',
             [
-                'listings' => Listing::all(),
+                'listings' => Listing::orderByDesc('created_at')
+                    ->paginate(10),
             ]
         );
     }
---
+++ resources/js/Components/UI/Pagination.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="flex gap-1">
+		<Link
+			v-for="(link, index) in links"
+			:key="index"
+			:href="link.url"
+			class="py-2 px4 rounded-md"
+			:class="{ 'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active }">
+			{{ link.label }}
+		</Link>
+	</div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+Link;
+defineProps({
+	links: {
+		Array,
+		default: [],
+	},
+});
+</script>
